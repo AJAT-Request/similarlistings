f685a5c48ea729f30dcc15358c6ec313c866052f
Tinh <tinhkle@Doxie.local>
2018-09-11 15:17:09 -0700

    added eslint for airbnb, seeding functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,8 @@
 
 module.exports = {
   extends: './node_modules/eslint-config-airbnb/index.js'
+  "env": {
+  	"browser": true,
+  	"node": true
+  }
 };
--- database/seed_functions/seed.js
+++ database/seed_functions/seed.js
@@ -1,24 +1,61 @@
 const con = require ('../index.js');
-const rooms = require('../data/roomData.js');
+const data = require('../data/roomData.js');
 const faker = require('faker');
 
-// const similiarListings = ['cabin', 'castle', 'cool house', 
-// 'glass house', 'grass house', 'hut', 'igloo', 'nice house', 
-// 'nicer house', 'rick and morty\'s house'];
+// ----------------- Math Helper Functions ------------------ //
 
-var seedRooms = function(rooms) {
-  var queryString = 'insert into rooms(name) values(?)';
-  var queryArgs = rooms;
+var getRandomNum = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var getRandomDecimal = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return ((Math.random() * (max - min)) + min).toFixed(1);
+}
+
+// --------------------------------------------------------- //
+
+var seedRooms = function() {
+  var queryString = 'insert into rooms(name) values ?';
+  var queryArgs = data.rooms;
 
   con.query(queryString, [queryArgs], function(err, results) {
-    console.log(results);
+  	if (err) {
+      console.log(err)
+  	}
+    console.log('Added ' + results.affectedRows + ' rows');
   });
-
 }
 
-var seedSimilarListings = function(similiarListings) {
-  
+var getListings = function() {
+  var listings = [];
+  var photoUrls = data.photoUrls;
+
+  for (var i = 0; i < 10; i++) {
+  	var name = faker.random.word();
+  	var basicInfo = faker.lorem.sentence();
+  	var pricePerNight = getRandomNum(50, 500);
+  	var averageRating = getRandomDecimal(3, 5);
+  	var randomIndex = getRandomNum(0, 10);
+  	var photoUrl = photoUrls[randomIndex];
+
+  	var listing = [name, basicInfo, pricePerNight, averageRating, photoUrl];
+  	listings.push(listing)
+  }
+
+  return listings;
 }
 
-seedRooms(rooms);
+var seedSimilarListings = function() {
+  var queryString = 'insert into similar_listings(name, basic_info, price_per_night, average_rating, image_url) values ?';
+  var queryArgs = getListings();
+
+  con.query(queryString, [queryArgs], function(err, results) {
+  	console.log('Added ' + results.affectedRows + ' rows');
+  });
+}
+
 
