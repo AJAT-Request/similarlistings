1069a7055b2011c4251e9b253f9f89483e3ac37f
Tinh <tinhkle@Doxie.local>
2018-09-12 11:45:26 -0700

    installed webpack, babel, created webpack config file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require ('body-parser');
 const app = express();
 
 app.use( bodyParser.json() );
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/../client/dist'))
 
 const PORT = 3000;
 app.listen(PORT, function() {console.log('Listening on port: ' + PORT)});
