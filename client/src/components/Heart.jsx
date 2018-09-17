f38eba1161022d87f76b4f5b3c39c91df1cd7cb8
Tinh <tinhkle@Doxie.local>
2018-09-17 11:55:52 -0700

    added heart icon on top of listing image using z-index, added another style component (listingInfo)

---
+++ client/src/components/Heart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeartIcon = () => (
+  <i className="far fa-heart" />
+);
+
+export default HeartIcon;
---
+++ client/src/components/HeartIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Heart } from '../../../css/styles.jsx';
+
+const HeartIcon = () => (
+  <Heart>
+    <i className="far fa-heart" />
+  </Heart>
+);
+
+export default HeartIcon;
